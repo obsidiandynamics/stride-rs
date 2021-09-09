ef597c66c69ab34c876cfb9e5af4a71c11a92bf5
Emil Koutanov <ekoutanov@gmail.com>
2021-09-09 15:30:11 +1000

    Added soak script

---
+++ sh/soak.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ "$SOAK_CMD" == "" ]; then
+  echo "SOAK_CMD is not set"
+  exit 1
+fi
+if [ "$SOAK_RUNS" == "" ]; then
+  SOAK_RUNS=1
+fi
+if [ "$SOAK_INTERVAL" == "" ]; then
+  SOAK_INTERVAL=0
+fi
+
+if [ "$SOAK_GITPULL" == "" ]; then
+  SOAK_GITPULL=1
+fi
+
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+GREY='\033[0;90m'
+NC='\033[0m'
+
+echo -e "${GREY}SOAK_CMD:      $SOAK_CMD${NC}"
+echo -e "${GREY}SOAK_RUNS:     $SOAK_RUNS${NC}"
+echo -e "${GREY}SOAK_INTERVAL: $SOAK_INTERVAL${NC}"
+echo -e "${GREY}SOAK_GITPULL:  $SOAK_GITPULL${NC}"
+
+cd "$(dirname $0)"/..
+
+set -e
+cycle=1
+while true ; do
+  echo -e "${CYAN}=============================================================================================================${NC}"
+  echo -e "${CYAN}Cycle ${cycle}${NC}"
+
+  for run in $(seq 1 $SOAK_RUNS)
+  do
+    timestamp=$(date +%'F %H:%M:%S')
+    echo -e "${GREEN}-------------------------------------------------------------------------------------------------------------${NC}"
+    echo -e "${GREEN}${timestamp}: Starting run ${run}/${SOAK_RUNS}${NC}"
+    echo -e "${GREEN}-------------------------------------------------------------------------------------------------------------${NC}"
+    $SOAK_CMD
+
+    sleep $SOAK_INTERVAL
+  done
+
+  if [ $SOAK_GITPULL == "1" ]; then
+    git pull
+  fi
+  cycle=$((cycle + 1))
+done
+
